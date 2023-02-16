c1db81a15eb4bd93927211d707991d6439370cfa
Aatish1111 <aatishkumarmoharana@gmail.com>
2023-02-16 20:57:22 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+
+const About = () => {
+  const data = {
+    name: "Thapa Ecommerce",
+  };
+  return (
+    <HeroSection myData={data}  />
+  )
+}
+
+export default About
